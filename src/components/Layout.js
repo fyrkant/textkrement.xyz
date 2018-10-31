c2403a7344305faeaada316ef81dd08c2818c550
Mattias <mattias.wikstrom@ephox.com>
2018-10-31 23:25:51 +0100

    remove stuff

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="textkrement.xyz" />
     <Navbar />
     <div>{children}</div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default class IndexPage extends React.Component {
                     <br />
                     <br />
                     <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
+                      Visa mer →
                     </Link>
                   </p>
                 </div>
