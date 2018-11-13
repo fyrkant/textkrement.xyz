3afb6d99b9e3436cfe818a77abf296b3d528c319
Mattias <mattias.wikstrom@ephox.com>
2018-11-13 21:29:46 +0100

    make about page look gooder

--- src/templates/about-page.tsx
+++ src/templates/about-page.tsx
@@ -9,13 +9,11 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
 
   return (
     <section className='section section--gradient'>
-      <div className='container'>
-        <div className='section'>
-          <h2 className='title is-size-3 has-text-weight-bold is-bold-light'>
-            {title}
-          </h2>
-          <PageContent className='content' content={content} />
-        </div>
+      <div className='container content'>
+        <h2 className='title is-size-3 has-text-weight-bold is-bold-light'>
+          {title}
+        </h2>
+        <PageContent className='content' content={content} />
       </div>
     </section>
   );
